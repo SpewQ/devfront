e377541eac08016d8535eb5dd27bad8d1a98db12
SpewQ <speewq@gmail.com>
2026-04-10 16:48:57 +1100

    v1

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+//App.tsx
+import Users from "./features/users/get-users/users.tsx"
+
+export function App() {
+  return (
+    <div className="flex min-h-svh p-6">
+      <Users />
+    </div>
+  )
+}
+
+export default App
---
+++ src/features/users/get-users/user.tsx
@@ -0,0 +1,9 @@
+import type { User } from "./user.type"
+
+export default function UserItem({ user }: { user: User }) {
+    return (
+        <p>
+            {user.name}
+        </p>
+    )
+}
