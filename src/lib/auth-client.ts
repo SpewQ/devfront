38ccff24fd78cc8b00d3443595020e7ea8d05e04
SpewQ <speewq@gmail.com>
2026-05-18 11:15:13 +1100

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-  baseURL: http://15.235.206.148:5001
+  baseURL: http://15.235.206.148:5001,
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
